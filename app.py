da65c71d636b9f95da5e5886a0275a7a0336f939
UYWASAS <uywasas@gmail.com>
2026-02-26 12:09:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def render_sidebar():
         st.markdown("<div class='uywa-logo-card'>", unsafe_allow_html=True)
         c1, c2, c3 = st.columns([1, 3, 1])
         with c2:
-            st.image("assets/logo.png", width=220)
+            st.image("assets/logo.png", width=350)
         st.markdown("</div>", unsafe_allow_html=True)
 
         st.markdown(
